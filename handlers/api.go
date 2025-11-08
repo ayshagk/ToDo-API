d0ce0def86686bff5dba25c1f655d3228d5413bd
catalinaaxsantana@gmail.com <catalinaaxsantana@gmail.com>
2025-11-08 19:21:59 +0000

    Register and login func done

--- handlers/api.go
+++ handlers/api.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"todoapi/database"
+	"todoapi/middleware"
 	"todoapi/models"
 	"todoapi/utils"
 )
@@ -43,10 +44,41 @@ func RegisterUser(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func Login() {
+func Login(w http.ResponseWriter, r *http.Request) {
+ //decode the request from the request body which is the email and pass 
+ var login models.Login
+ err := json.NewDecoder(r.Body).Decode(&login)
+ if err != nil{
+	http.Error(w, err.Error(), http.StatusBadRequest)
+	return 
+ }
 
+ //check if the user exists when logging on 
+ var user models.User
+ err = database.Db.Where("email = ?", login.Email).First(&user).Error
+ if err != nil {
+	http.Error(w, "user not found", http.StatusBadRequest)
+ return 
 }
 
+//check if password matches the one in our db 
+err = utils.ComparePassword(login.Password, user.Password)
+if err != nil{
+	http.Error(w, "invalid password", http.StatusBadRequest)
+	return 
+}
+
+//generate a jwt token
+token, err:=middleware.GenerateJWT(user.ID)
+if err != nil{
+	http.Error(w, "failed to generate token", http.StatusInternalServerError)
+return
+}
+
+w.WriteHeader(200)
+json.NewEncoder(w).Encode(token)
+}
+/*
 func CreateToDo(){
 
 }
@@ -63,3 +95,4 @@ func UpdateToDo(){
 func DeleteToDo(){
 
 }
+*/
